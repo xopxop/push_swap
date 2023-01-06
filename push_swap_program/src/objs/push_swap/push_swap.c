4e62c9fdfc5dfe4f08bba1ced40e1ac13f23cd76
du than <du.than@relexsolutions.com>
2023-01-06 17:33:02 +0200

    fixed logic

--- push_swap_program/src/objs/push_swap/push_swap.c
+++ push_swap_program/src/objs/push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: dthan <dthan@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 12:29:02 by dthan             #+#    #+#             */
-/*   Updated: 2022/12/25 22:51:51 by dthan            ###   ########.fr       */
+/*   Updated: 2023/01/06 14:24:07 by dthan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,15 +41,13 @@ int can_rotate(t_stack *stack, int order)
 		if (order == ASCENDING)
 		{
 			if (stack->first_node->data > stack->first_node->next->data && \
-			stack->first_node->data > stack->last_node->data && \
-			stack->first_node->next->data < stack->last_node->data)
+			stack->first_node->data > stack->last_node->data)
 				return (1);
 		}
 		else if (order == DESCENDING)
 		{
 			if (stack->first_node->data < stack->first_node->next->data && \
-			stack->first_node->data < stack->last_node->data && \
-			stack->first_node->next->data > stack->last_node->data)
+			stack->first_node->data < stack->last_node->data)
 				return (1);
 		}
 	}
@@ -62,17 +60,41 @@ int	can_reverse_rotate(t_stack *stack, int order)
 	{
 		if (order == ASCENDING)
 		{
-			if (stack->first_node->data > stack->last_node->data)
+			if (stack->first_node->data > stack->last_node->data && \
+			stack->first_node->data < stack->first_node->next->data)
 				return (1);
 		}
 		else if (order == DESCENDING)
 		{
-			if (stack->first_node->data < stack->last_node->data)
+			if (stack->first_node->data < stack->last_node->data && \
+			stack->first_node->data > stack->first_node->next->data)
 				return (1);
 		}
 	}
 	return (0);
 }
+
+int can_swap_simple(t_stack *stack, int order)
+{
+	if (stack->count > 1)
+	{
+		if (order == ASCENDING)
+		{
+			if (stack->first_node->data > stack->first_node->next->data)
+				return (1);
+		}
+		else if (order == DESCENDING)
+		{
+			if (stack->first_node->data < stack->first_node->next->data)
+				return (1);
+		}
+	}
+	return (0);
+}
+
+// rotate: first > next && first < last (condition n > 2)
+// reverse rotate: first < next && first > last (condition n > 2)
+// swap: first > next (condition n > 1)
 
 char *push_swap_generate_instructions2(t_stack *stack_a, t_stack *stack_b)
 {
@@ -83,7 +105,10 @@ char *push_swap_generate_instructions2(t_stack *stack_a, t_stack *stack_b)
 			return (ft_strdup("rrr"));
 		else if (can_swap(stack_a, ASCENDING) && can_swap(stack_b, DESCENDING))
 			return (ft_strdup("ss"));
-		return (ft_strdup("pb"));
+		if (stack_a->count > stack_b->count) {
+			return (push_swap_generate_instructions2(NULL, stack_b));
+		}
+		return (push_swap_generate_instructions2(stack_a, NULL));
 	} else if (stack_a) {
 		if (can_rotate(stack_a, ASCENDING))
 			return (ft_strdup("ra"));
@@ -101,7 +126,7 @@ char *push_swap_generate_instructions2(t_stack *stack_a, t_stack *stack_b)
 			return (ft_strdup("sb"));
 		return (ft_strdup("pa"));
 	}
-	return ft_strdup("pb");
+	return ft_strdup("pa");
 }
 
 char *push_swap_generate_instructions(t_stack *stack_a, t_stack *stack_b)
@@ -149,16 +174,16 @@ void push_swap_run(t_push_swap *program)
 	{
 		if (stack_is_empty(stack_b) && stack_is_ascending_sorted(stack_a))
 			break ;
-		ft_printf("STACK A:\n");
-		print_stack(stack_a);
-		ft_printf("STACK B:\n");
-		print_stack(stack_b);
+		// ft_printf("STACK A:\n");
+		// print_stack(stack_a);
+		// ft_printf("STACK B:\n");
+		// print_stack(stack_b);
 		instruction = push_swap_generate_instructions(stack_a, stack_b);
-		ft_printf("%s\n", instruction);
+		// ft_printf("%s\n", instruction);
 		execute_instruction(stack_a, stack_b, instruction);
 		free(instruction);
 		i++;
-		sleep(1);
+		// sleep(1);
 	}
 	ft_printf("Total: %d\n", i);
 }
